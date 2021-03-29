62327c1d62a2cbe07f36e1deb5c01b8177a577d5
nick <nicholas_scalione@yahoo.com>
2021-03-29 12:51:06 -0400

    commit tree ceba76d9483cbb6f10120de74c021b8f608ec22c

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -3,11 +3,12 @@ class Classification < ActiveRecord::Base
   has_many :boats, through: :boat_classifications
 
   def self.my_all
-    # all
+    all
   end
 
   def self.longest
-    # Boat.longest.classifications
+    Classification.includes(:boats).where(boats:
+      {length: Boat.maximum('length')}
+    )
   end
-
 end
